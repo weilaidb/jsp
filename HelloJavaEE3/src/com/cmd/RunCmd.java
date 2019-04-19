437bd354ae603752c0c04e31dedc13ecf128c978
weilaidb <wxjlmr@126.com>
2019-04-20 00:31:43 +0800

    Squashed commit of the following:
    commit 46f44330dcf6b15088f33db049815dfe43f106d7
    Author: weilaidb <wxjlmr@126.com>
    Date:   Sat Apr 20 00:05:58 2019 +0800
    
        添加必备修改
    
    commit 1279a896e9c617ed217ad20472b08958632d8ba4
    Author: weilaidb <wxjlmr@126.com>
    Date:   Fri Apr 19 23:08:29 2019 +0800
    
        添加文件打开路径
    
    commit 445e8e0b620c2dd0ee72c7a1a68f3de767d7d85b
    Author: weilaidb <wxjlmr@126.com>
    Date:   Fri Apr 19 22:58:45 2019 +0800
    
        打开文件页面
    
    # Conflicts:
    #	HelloJavaEE3/.idea/workspace.xml
    #	HelloJavaEE3/src/com/cmd/RunCmd.java
    #	SprintTest/.idea/workspace.xml
    
    # Conflicts:
    #	HelloJavaEE3/.idea/workspace.xml
    #	HelloJavaEE3/src/com/cmd/RunCmd.java
    #	SprintTest/.idea/workspace.xml

--- HelloJavaEE3/src/com/cmd/RunCmd.java
+++ HelloJavaEE3/src/com/cmd/RunCmd.java
@@ -37,28 +37,28 @@ public class RunCmd {
             e.printStackTrace();
         }
 
-////##读取命令的输出信息
-//        BufferedReader reader = null;
-//        try {
-//            InputStream is = p.getInputStream();
-//            reader = new BufferedReader(new InputStreamReader(is));
-//            p.waitFor();
-//            if (p.exitValue() != 0) {
-//                //说明命令执行失败
-//                //可以进入到错误处理步骤中
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-////##打印输出信息
-//        try {
-//            String s = null;
-//            while ((s = reader.readLine()) != null) {
-//                System.out.println(s);
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+//##读取命令的输出信息
+        BufferedReader reader = null;
+        try {
+            InputStream is = p.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(is));
+            p.waitFor();
+            if (p.exitValue() != 0) {
+                //说明命令执行失败
+                //可以进入到错误处理步骤中
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//##打印输出信息
+        try {
+            String s = null;
+            while ((s = reader.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
