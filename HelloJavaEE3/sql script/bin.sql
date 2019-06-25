07ac6405d2a286e049c8fe6b7f0703f1b0054aed
weilaidb <wxjlmr@126.com>
2019-06-25 23:44:11 +0800

    生成二进制数据库及表

---
+++ HelloJavaEE3/sql script/bin.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `abcbin` (
+  `id` int(200) unsigned NOT NULL AUTO_INCREMENT,
+  `name` tinyblob NOT NULL,
+  `content` longblob NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
