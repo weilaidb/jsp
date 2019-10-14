2fd799d9059ad53b74bb269cfaf5aede3980abba
yjduoduo <wxjlmr@126.com>
2019-10-14 23:46:26 +0800

    处理表情编码，需要修改数据库编码为utf8mb4

---
+++ HelloJavaEE3/sql script/change_character.sql
@@ -0,0 +1,6 @@
+use alldb;
+ALTER TABLE `abc` DEFAULT CHARACTER SET utf8mb4;
+alter table `abc` convert to character set utf8mb4;
+use alldbbin;
+ALTER TABLE `abcbin` DEFAULT CHARACTER SET utf8mb4;
+alter table `abcbin` convert to character set utf8mb4;
