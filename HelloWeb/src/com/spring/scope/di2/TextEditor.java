167a159f2c18f5e33ff4209f2bea239ec63255da
yjduoduo <wxjlmr@126.com>
2020-01-18 14:41:07 +0800

    di2

---
+++ HelloWeb/src/com/spring/scope/di2/TextEditor.java
@@ -0,0 +1,22 @@
+package com.spring.scope.di2;
+
+public class TextEditor {
+    private SpellChecker spellChecker;
+    private SpellChecker spellChecker2;
+    // a setter method to inject the dependency.
+    public void setSpellChecker(SpellChecker spellChecker2) {
+        System.out.println("Inside setSpellChecker." );
+        this.spellChecker = spellChecker2;
+    }
+    // a getter method to return spellChecker
+    public SpellChecker getSpellChecker() {
+        return spellChecker;
+    }
+    public void spellCheck() {
+        spellChecker.checkSpelling();
+    }
+    public void setSpellChecker2(SpellChecker spellChecker2) {
+        System.out.println("Inside setSpellChecker2." );
+        this.spellChecker2 = spellChecker2;
+    }
+}
