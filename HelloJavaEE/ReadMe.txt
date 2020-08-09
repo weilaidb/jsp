17c34cf6e5e9bea74eb30eff15a194ef800e5667
yjduoduo <wxjlmr@126.com>
2020-08-09 23:16:42 +0800

    delete no use

--- HelloJavaEE/ReadMe.txt
+++
@@ -1,5 +0,0 @@
-HelloWrold Servert的访问地址
-http://localhost:8080/hello/HelloWorld
-
-index.jsp的访问地址 
-http://localhost:8080/hello/index.jsp
--- SprintTest/src/com/mldn/lxh/vo/DatabaseConnection.java
+++
@@ -1,8 +0,0 @@
-package com.mldn.lxh.vo;
-
-import java.sql.Connection;
-
-public interface DatabaseConnection {
-    public Connection getConnection();
-    public void close();
-}
